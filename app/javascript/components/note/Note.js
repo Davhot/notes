df7ae853691fe467d97bded6c6f72c1bb3fd1e9e
Хотелов Давид Александрович <dhotelov@stdev.ru>
2019-11-24 18:46:45 +0300

    fix long press notes on mobile

--- app/javascript/components/note/Note.js
+++ app/javascript/components/note/Note.js
@@ -6,6 +6,12 @@ import NoteForm from './NoteForm'
 import { setMode, selectNotes, setCurrentNoteId } from "./NoteActions"
 
 class Note extends React.Component {
+  constructor() {
+    super()
+    this.handleButtonPress = this.handleButtonPress.bind(this)
+    this.handleButtonRelease = this.handleButtonRelease.bind(this)
+  }
+
   redirect_to_notes() {
     if (this.props.mode == 'index') {
       this.props.setMode('edit');
@@ -19,17 +25,13 @@ class Note extends React.Component {
     }
   }
 
-  long_press_card_mouseup() {
-    clearTimeout(this.pressTimer);
-    return false;
+  handleButtonPress() {
+    const self = this;
+    this.buttonPressTimer = setTimeout(() => self.props.setMode('delete'), 500);
   }
 
-  long_press_card_mousedown() {
-    const self = this;
-    this.pressTimer = window.setTimeout(function() {
-      self.props.setMode('delete');
-    }, 500);
-    return false;
+  handleButtonRelease () {
+    clearTimeout(this.buttonPressTimer);
   }
 
   render () {
@@ -37,9 +39,12 @@ class Note extends React.Component {
     if (this.props.note.selected) { note_class += " checked" }
     return (
       <div className={note_class}
-           onMouseUp={() => this.long_press_card_mouseup()}
-           onMouseDown={() => this.long_press_card_mousedown()}
-           onClick={() => this.redirect_to_notes()}>
+           onClick={() => this.redirect_to_notes()}
+           onTouchStart={this.handleButtonPress}
+           onTouchEnd={this.handleButtonRelease}
+           onMouseDown={this.handleButtonPress}
+           onMouseUp={this.handleButtonRelease}
+           onMouseLeave={this.handleButtonRelease}>
         <p className="notes-content">{this.props.note.body}</p>
       </div>
     )
